9c4bda1996209ccf74ab44d0353c0d1273ab913f
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-04 09:41:47 -0500

    req res json tested

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,5 +1,6 @@
 const TodoModel = require('../model/todo.model');
 
 exports.createTodo = (req, res, next) => {
-  TodoModel.create(req.body);
+  const createModel = TodoModel.create(req.body);
+  res.status(201).json(createModel);
 };
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -5,17 +5,37 @@ const newTodo = require('../mock-data/new-todo.json');
 
 TodoModel.create = jest.fn();
 
+let req, res, next;
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 describe('TodoController.createTodo', () => {
+  beforeEach(() => {
+    req.body = newTodo;
+  });
+
   it('should have a createTodo function', () => {
     expect(typeof TodoController.createTodo).toBe('function');
   });
 
   it('should call TodoModel.create', () => {
-    let req = httpMocks.createRequest();
-    let res = httpMocks.createResponse();
-    let next = null;
-    req.body = newTodo;
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(newTodo);
+  });
+
+  it('should return 201 response code', () => {
+    TodoController.createTodo(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it('should return json body in response', () => {
+    TodoModel.create.mockReturnValue(newTodo);
+    TodoController.createTodo(req, res, next);
+    expect(res._getJSONData()).toBe(newTodo);
   });
 });
