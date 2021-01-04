9e896f7752e5c7a1094932ee42f5d3603c791019
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-04 11:34:00 -0500

    refactor integration test

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const todoRoutes = require('./routes/todo.routes');
 
+app.use(express.json);
+
 app.use('/todos', todoRoutes);
 
 app.get('/', (req, res) => {
   res.json('Hi');
 });
 
-app.listen(3001, () => {
-  console.log('Server is now running.');
-});
+// app.listen(3001, () => {
+//   console.log('Server is now running.');
+// });
 
 module.exports = app;
