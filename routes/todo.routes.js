cfacc2387c162c8b150451ccdc6cf858347b959b
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-05 11:04:43 -0500

    added integration test for get

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const todoController = require('../controllers/todo.controller');
 
 router.post('/', todoController.createTodo);
+router.get('/', todoController.getTodos);
 
 module.exports = router;
