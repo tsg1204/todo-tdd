630cf2685ff447e2c0c6a46d90534d5aa95c9fba
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-05 10:41:24 -0500

    test for get method added

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -8,3 +8,7 @@ exports.createTodo = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getTodos = async (req, res, next) => {
+  TodoModel.find({});
+};
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -4,6 +4,7 @@ const httpMocks = require('node-mocks-http');
 const newTodo = require('../mock-data/new-todo.json');
 
 TodoModel.create = jest.fn();
+TodoModel.find = jest.fn();
 
 let req, res, next;
 
@@ -11,6 +12,17 @@ beforeEach(() => {
   req = httpMocks.createRequest();
   res = httpMocks.createResponse();
   next = jest.fn();
+});
+
+describe('TodoController.getTodos', () => {
+  it('should have a getTodos function', () => {
+    expect(typeof TodoController.getTodos).toBe('function');
+  });
+
+  it('should call TodoModel.find({})', async () => {
+    await TodoController.getTodos(req, res, next);
+    expect(TodoModel.find).toHaveBeenCalledWith({});
+  });
 });
 
 describe('TodoController.createTodo', () => {
